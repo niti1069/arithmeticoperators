a001fcd8707d6f62a5c022c055dc028be354a925
niti jashvir <njashvir@yahoo.co.uk>
2023-10-28 12:25:16 +0100

    this is my arithmetic operations homework

---
+++ ArithmeticOperators.java
@@ -0,0 +1,21 @@
+package arithmeticoperations;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int A = 100;
+        int B = 20;
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A++);
+        System.out.println(--B);
+
+    }
+
+
+
+
+
+
+    }
